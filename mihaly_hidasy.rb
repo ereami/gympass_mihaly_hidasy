6c707db57abd55461e8640cf7eaca395146f440e
Elderclei Reami <elderclei.reami@gmail.com>
2019-06-12 10:03:30 -0300

    Computing grid positions

--- mihaly_hidasy.rb
+++ mihaly_hidasy.rb
@@ -8,6 +8,8 @@ end
 
 File.open(ARGV.first, "r") do |f|
   race = Race.new
+  
+  pilot = {}
   f.each_line.with_index do |line, line_num|
     break if race.finished?
     next if line_num == 0 || line.chomp.length == 0
@@ -18,11 +20,21 @@ File.open(ARGV.first, "r") do |f|
 
     lap_timing_ms = LapData.convert_lap_timing_string(lap_timing)
 
-    print line_num, " - ", pilot_code, " - ", lap_timing_ms, "\n"
-    puts race.finished?
+    pilot[pilot_code] = { :pilot_name => pilot_name, :lap_num => lap_num, :total_time => 0 } if pilot[pilot_code].nil?
+    pilot[pilot_code][:lap_num] = lap_num
+    pilot[pilot_code][:total_time] =  pilot[pilot_code][:total_time] + lap_timing_ms  
+
+  end
+
+  puts "\u{1f3c1}  Terminada a corrida!"
+  final_grid = pilot.sort_by { |code, pilot_data| -pilot_data[:total_time] }
+  
+  puts "Pos\tCod.Piloto\tNom.Piloto\t\tNo.Voltas\tTempo Total"
+  final_grid.each_with_index do |position_data, position|
+    pilot_code = position_data.first
+    pilot_data = position_data.last
+    puts "#{position + 1} \t #{pilot_code}\t#{pilot_data[:pilot_name]}\t\t#{pilot_data[:lap_num]}\t#{pilot_data[:total_time]}"
   end
 end
 
-
-puts "\u{1f3c1}  Terminada a corrida!"
 
